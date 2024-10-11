8141e6a82c12008ad87e133aa64cbc8f142bc718
sultonmurodova09 <oysultonsultonmurodova@gmail.com>
2024-10-11 15:49:48 +0500

    Errors

---
+++ errors.py
@@ -0,0 +1,77 @@
+#SyntaxError!
+
+print "Hello World!"
+
+#EOL xatolik
+
+print("Hello World!
+      
+#EOF xatolik
+
+print("Hello World!"
+      
+#IndentationError!
+
+print("O'ngacha sanaymiz")
+for n in range(10):
+print(n+1)
+
+son = 50
+if son>=0:
+    print("Musbat son")
+else:
+print("Manfiy son")
+
+#TypeError
+
+son = input("Istalgan son kiriting: ")
+print(f"{son} ning kvadrati {son**2} ga teng")
+
+#NameError!
+
+prit("Hello World!")
+
+mevalar = ['olma','uzum','nok','anor','anjir']
+for meva in mvealar:
+    print(meva)
+
+#ValueError!
+
+son = int(input("Istalgan son kiriting: "))
+if son>=0:
+    print("Musbat son")
+else:
+    print("Manfiy son")
+
+#IndexError!
+
+mevalar = ['olma','anor','uzum']
+print(mevalar[3])
+
+mevalar = ['olma','anor','uzum']
+print(mevalar[2])
+
+#ZeroDevisionError!
+
+x, y = 50, 50
+z = 250/(x-y)
+
+#Mantiqiy xatolar!
+
+radius = 5
+pi = 4.14
+aylana_yuzi = pi*radius**2
+print(aylana_yuzi)
+
+son = float(input("Istalgan son kiriting: "))
+ildiz = son**1/2
+print(f"{son} ning ildizi {ildiz} ga teng")
+
+son = float(input("Istalgan son kiriting: "))
+ildiz = son**(1/2)
+print(f"{son} ning ildizi {ildiz} ga teng")
+
+mevalar = ['olma','uzum','nok','anor','anjir']
+for meva in mevalar:
+    print(meva)
+    print("Dastur tugadi")
