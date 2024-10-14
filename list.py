52a593592787903c680412760495c5bde2acd38c
sultonmurodova09 <oysultonsultonmurodova@gmail.com>
2024-10-14 12:02:59 +0500

    list(ro'yxat)

---
+++ list.py
@@ -0,0 +1,87 @@
+# List quyidagicha yaratiladi:
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+ismlar = [] # bo'sh ro'yxat
+
+# LIST ELEMENTLARI
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+print("Birinchi meva: ", mevalar[0])
+print("Ikkinchi meva: ", mevalar[1])
+
+# Agar list ichidagi elementlar matn ko'rinishid bo'lsa, ularga string metodlarni qo'llashimiz mumkin
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+print("Birinchi meva: ", mevalar[0].title())
+print("Ikkinchi meva: ", mevalar[1].upper())
+
+# List elementlari ustida arifmetik amallar bajarish:
+
+narhlar = [12000, 18000, 10900, 22000]
+print(narhlar[2] + narhlar[3])
+
+# Pythonda Listning eng oxirgi elementiga -1 indeksi orqali ham murojat qilish mumkin.
+# Bu usul Listning uzunligini bilmaganda juda asqotadi.
+
+car_models = ['Toyota', 'GM', 'Volvo', 'BMW', 'Hyundai', 'Kia', 'Volkswagen']
+print(car_models[-1]) # Listning eng oxirgi elementiga -1 bilan murojat qilamiz 
+
+#  Elementni o'zgartirish
+
+narhlar = [12000, 18000, 10900, 22000]
+narhlar[0] = 13000 # 1-qiymatni 13000 ga o'zgartiramiz
+narhlar[2] = 11000 # 3-qiymatni 11000 ga o'zgartiramiz
+narhlar[3] = narhlar[3]+2000 # 4-qiymatga 2000 qo'shamiz
+print(narhlar)
+
+# Yangi element qo'shish
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+mevalar.append("tarvuz") # mevalar ga tarvuz qo'shamiz
+print(mevalar)
+
+# bo'sh ro'yxatga yangi element qo'shish
+
+cars = [] # bo'sh ro'yxat yaratamiz
+cars.append('Lacetti') # ro'yxatga Lacetti mashinasini qo'shamiz
+cars.append('Nexia 3') # ro'yxatga Nexia 3 mashinasini qo'shamiz
+cars.append('Cobalt')  # ro'yxatga Cobalt  mashinasini qo'shamiz
+print(cars)
+
+# .insert() metodi
+
+cars = ['Lacetti', 'Nexia 3', 'Cobalt']
+cars.insert(0, 'Malibu') # 1-o'ringa yangi qiymat qo'shamiz
+print(cars)
+
+cars.insert(2, 'Damas') # 3-o'ringa yangi qiymat qo'shamiz
+print(cars)
+
+# Elementni o'chirish
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik", 'anor']
+del mevalar[1] # 2-element (anjir) ni o'chirib tashlaymiz
+print(mevalar)
+
+# .remove(qiymat) metodi
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik", 'anor']
+mevalar.remove('shaftoli') # Ro'yxatdan shaftolini o'chirdik
+print(mevalar)
+
+hayvonlar = ['it', 'mushuk', 'sigir', 'qo\'y', 'quyon', 'mushuk']
+hayvonlar.remove("mushuk") # Ro'yxatda 2 ta mushuk bor, ulardan birinchisi o'chadi
+print(hayvonlar)
+
+# Elementni sug'urib olish
+
+bozorlik = ["yog'", 'un', 'piyoz', 'banan', "go'sht"]
+mahsulot = bozorlik.pop(3) # Ro'yxatdan banan ni sug'urib olamiz
+print("Men " + mahsulot + " sotib oldim")
+print("Olinmagan mahsulotlar: ", bozorlik)
+
+
+
+
