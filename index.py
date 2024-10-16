73b27d55f464d3766c7abc4ea79ad2fd9fafa3de
sultonmurodova09 <oysultonsultonmurodova@gmail.com>
2024-10-16 16:01:02 +0500

    fizbuz masalasi

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
-texts=input("Ixtiyoriy matn kiriting: ")
-print(len(texts))
+# texts=input("Ixtiyoriy matn kiriting: ")
+# print(len(texts))
 
 
 numbers=list(range(1,50))
 for number in range(1,50):
-    if number%3 ==0:
-        print("fiz")
-    if number %5==0:
-        print("buz")
-    if number%3==0 and number%5==0:
+    if number %3 == 0 and number %5 == 0:
         print("fizbuz")
+    elif number %5 == 0:
+        print("buz")
+    elif number %3 == 0:
+        print("fiz")
+
+
 
