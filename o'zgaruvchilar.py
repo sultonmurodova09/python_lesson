5e42e7c64695159a7ac59388df28b1369dfb596c
sultonmurodova09 <oysultonsultonmurodova@gmail.com>
2024-11-09 22:17:37 +0500

    O'zgaruvchilar

---
+++ o'zgaruvchilar.py
@@ -0,0 +1,42 @@
+"""O'ZGARUVCHI (VARIABLE)"""
+
+ism = "Abdulloh"
+yosh = 25
+print(ism)
+print(yosh)
+
+ism = "Abdulloh"
+print(ism)
+ism="Muhammad"
+print(ism)
+
+
+
+"""O'ZGARUVCHILARNI NOMLASH"""
+
+
+# O'zgaruvchilarga nom berishda quyidagi qoidalarga amal qiling:
+
+# O'zgaruvchi nomi harf yoki pastki chiziq (_) bilan boshlanishi kerak
+
+# O'zgaruvchi nomi raqam bilan boshlanishi mumkin emas
+
+# O'zgaruvchi nomida faqatgina lotin alifbosi harflari (A-z), raqamlar (0-9) va pastki chiziq (_) qatnashishi mumkin
+
+# O'zgaruvchi nomida bo'shliq (пробел) bo'lishi mumkin emas
+
+# O'zgaruvchi nomida katta-kichik harflar turlicha talqin qilinadi (ism, ISM, va Ism uchta turli o'zgaruvchi)
+
+
+"""o'shimcha qoida sifatida: """
+
+
+# O'zgaruvchi nomini kichik harflar bilan yozing. 
+
+# O'zgaruvchi nomida 2 va undan ortiq so'z qatnashsa ularning orasini pastki chiziq (_) bilan ajrating (ism_sharif="Anvar Narzullaev")
+
+# O'zgaruvchiga tushunarli nom bering (y=20 emas yosh=20, d="Korea" emas davlat = "Korea" va hokazo)
+
+# Shuningdek o'zgaruvchilarga Pythonda ishlatiladigan funktsiyalar va maxsus kalit so'zlarning (keywords) nomini bermang. 
+# Kalit so'zlar ro'yhatini ko'rish uchun Spyder konsolida avval help() deb yozing va Enter tugmasini bosing. 
+# Keyin esa keywords deb kiritib, yana Enter bosing. 
